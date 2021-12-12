61dcff7a57d901104b1237ceea7b471af4b62011
rchitecture <95155049+rchitecture@users.noreply.github.com>
2021-12-12 18:18:18 +0900

    Initial commit

---
+++ hello.py
@@ -0,0 +1,21 @@
+fruits = ['사과', '배', '배', '감', '수박', '귤', '딸기', '사과', '배', '수박']
+
+cnt = 0
+
+for fruit in fruits:
+    if fruit == '배':
+        cnt += 1
+
+
+print(cnt)
+
+def count_fruits(name):
+    cnt = 0
+
+    for fruit in fruits:
+        if fruit == 'name':
+            count += 1
+
+    return cnt
+
+print(count_fruits('사과'))
